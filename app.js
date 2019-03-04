f064dd7892680bb8dfb7ec3576a4d2732d37bba6
zendizmo <zendizmo@gmail.com>
2019-03-03 20:10:35 -0500

    added contacts and accounts

--- app.js
+++ app.js
@@ -4,12 +4,76 @@ const app = express();
 const config = require("./config/database");
 const { Pool, Client } = require("pg");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.get("/", function(req, res) {
+//   res.render("index");
+// });
+
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("index.ejs", {
+    title: "Home",
+    nav: ["Home", "Accounts", "Contacts"]
+  });
+});
+
+app.get("/home", function(req, res) {
+  res.render("index.ejs", {
+    title: "Home",
+    nav: ["Home", "Accounts", "Contacts"]
+  });
+});
+
+app.get("/account", function(req, res) {
+  try {
+    const connectionString = process.env.DATABASE_URL;
+    const client = new Client({
+      connectionString: connectionString
+    });
+    client.connect();
+    client.query(
+      "SELECT Id, Name, AccountNumber FROM salesforce.account",
+      (err, dbRes) => {
+        console.log(err, dbRes);
+        console.log(dbRes.rows);
+        res.render("account.ejs", {
+          title: "Account",
+          nav: ["Home", "Accounts", "Contacts"],
+          results: dbRes.rows
+        });
+        client.end();
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/contact", function(req, res) {
+  try {
+    const connectionString = process.env.DATABASE_URL;
+    const client = new Client({
+      connectionString: connectionString
+    });
+    client.connect();
+    client.query(
+      "SELECT name, email, title, description FROM salesforce.contact",
+      (err, dbRes) => {
+        console.log(err, dbRes);
+        console.log(dbRes.rows);
+        res.render("contact.ejs", {
+          title: "Contact",
+          nav: ["Home", "Accounts", "Contacts"],
+          results: dbRes.rows
+        });
+        client.end();
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const accounts = require("./routes/accounts");
